27a72f9778a17e3a577955476040f6e71d944929
Hidekazu Kondo <kaz@lunascape.org>
2019-03-16 16:44:50 +0900

    Update template to goemon v0.12.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    del = require('del');
 
 gulp.task('copy-template', function() {
   return gulp.src('./src/goemon/**/*')
     .pipe(gulp.dest('./app/template/'));
 });
 
-gulp.task('build', gulp.series('copy-template'));
+gulp.task('remove-tempalte', del.bind(null, ['app/template']));
+
+gulp.task('build', gulp.series('remove-tempalte', 'copy-template'));
 
 gulp.task('default', gulp.series('build'));
